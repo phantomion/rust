eea6713ee11709a88a3f587ccf05a5ae38acc615
Konstantinos Kyriakou <kyriakoukonstantinos1@gmail.com>
2022-11-12 19:01:20 +0200

    Print contextual effects after finding bb relationships

--- compiler/rustc_mir_transform/src/contextual_effects.rs
+++ compiler/rustc_mir_transform/src/contextual_effects.rs
@@ -1,5 +1,6 @@
 use crate::MirPass;
-use rustc_data_structures::fx::FxHashMap;
+use rustc_data_structures::fx::{FxHashMap, FxHashSet};
+use rustc_middle::mir::visit::Visitor;
 use rustc_middle::mir::*;
 use rustc_middle::ty::TyCtxt;
 
@@ -7,64 +8,126 @@ pub struct ContextualEffects;
 
 impl<'tcx> MirPass<'tcx> for ContextualEffects {
     fn run_pass(&self, _tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
-        debug!("contextual_effects({:?})", body.source.instance);
+        debug!("function {:?}", body.source.instance);
         self.generate_contextual_effects(body)
     }
 }
 
+static mut EFFECT_COUNTER: i64 = 0;
+
 impl ContextualEffects {
     fn generate_contextual_effects(&self, body: &mut Body<'_>) {
-        let mut effects = Effects::new();
+        let mut effects = BlockConstraints::new();
         let preorder: Vec<_> = traversal::preorder(body).map(|(bb, _)| bb).collect();
         for bb in preorder {
-            debug!("bb id {:?}", bb);
-            effects.current.insert(bb, body[bb].clone());
-            effects.generate_prior_effects(bb, body);
-            let future = effects.generate_future_effects(&body[bb]);
+            unsafe {
+                effects.current.insert(bb, EFFECT_COUNTER);
+            }
+            effects.generate_prior_blocks(bb, body);
+            let mut future = FxHashSet::default();
+            effects.generate_future_blocks(bb, body, &mut future);
             effects.future.insert(bb, future);
+            unsafe {
+                EFFECT_COUNTER += 1;
+            }
         }
-        debug!("prior: {:?}", effects.prior);
+        /* debug!("prior: {:?}", effects.prior);
+        debug!("future: {:?}", effects.future);
+        debug!("current: {:?}", effects.current); */
+        for (bb, effect) in effects.current.clone() {
+            debug!("ε_{} {{", effect);
+            effects.visit_basic_block_data(bb, &body[bb]);
+        }
+        for (bb, set) in effects.prior.clone() {
+            for block in set {
+                let set_block_effect = effects.current.get(&block).unwrap();
+                let curr_block_effect = effects.current.get(&bb).unwrap();
+                debug!("α_{} -> α_{}", curr_block_effect, set_block_effect);
+            }
+        }
+        for (bb, set) in effects.future.clone() {
+            for block in set {
+                let set_block_effect = effects.current.get(&block).unwrap();
+                let curr_block_effect = effects.current.get(&bb).unwrap();
+                debug!("ω_{} <- ω_{}", curr_block_effect, set_block_effect);
+            }
+        }
     }
 }
 
-struct Effects<'tcx> {
-    prior: FxHashMap<BasicBlock, Vec<BasicBlockData<'tcx>>>,
-    current: FxHashMap<BasicBlock, BasicBlockData<'tcx>>,
-    future: FxHashMap<BasicBlock, Vec<BasicBlockData<'tcx>>>,
+impl<'tcx> Visitor<'tcx> for BlockConstraints {
+    fn visit_statement(&mut self, statement: &Statement<'tcx>, _location: Location) {
+        debug!("{:?}", statement);
+    }
+
+    fn visit_terminator(&mut self, terminator: &Terminator<'tcx>, location: Location) {
+        debug!("}}");
+        match &terminator.kind {
+            TerminatorKind::Call { func, .. } => {
+                let block = self.current.get(&location.block).unwrap();
+                debug!("ε_{} -> ε_{:?}", block, func);
+            }
+            TerminatorKind::SwitchInt { .. } => {}
+            TerminatorKind::Drop { .. } => {}
+            TerminatorKind::Goto { .. }
+            | TerminatorKind::Resume
+            | TerminatorKind::Abort
+            | TerminatorKind::Return
+            | TerminatorKind::Unreachable
+            | TerminatorKind::DropAndReplace { .. }
+            | TerminatorKind::Assert { .. }
+            | TerminatorKind::Yield { .. }
+            | TerminatorKind::GeneratorDrop
+            | TerminatorKind::FalseEdge { .. }
+            | TerminatorKind::FalseUnwind { .. }
+            | TerminatorKind::InlineAsm { .. } => {}
+        }
+    }
 }
 
-impl<'tcx> Effects<'tcx> {
+struct BlockConstraints {
+    prior: FxHashMap<BasicBlock, FxHashSet<BasicBlock>>,
+    current: FxHashMap<BasicBlock, i64>,
+    future: FxHashMap<BasicBlock, FxHashSet<BasicBlock>>,
+}
+
+impl<'tcx> BlockConstraints {
     fn new() -> Self {
-        Effects {
+        BlockConstraints {
             prior: FxHashMap::default(),
             future: FxHashMap::default(),
             current: FxHashMap::default(),
         }
     }
 
-    fn generate_prior_effects(&mut self, bb: BasicBlock, body: &Body<'tcx>) {
+    fn generate_prior_blocks(&mut self, bb: BasicBlock, body: &Body<'tcx>) {
         for target in body[bb].terminator().successors() {
             let mut prior = match self.prior.get(&bb) {
                 Some(prior) => prior.clone(),
-                None => vec![],
+                None => FxHashSet::default(),
             };
-            match self.current.get(&bb) {
-                Some(current) => prior.push(current.clone()),
-                None => {}
-            }
+            prior.insert(bb);
             match self.prior.get_mut(&target) {
                 Some(prior_vec) => prior_vec.extend(prior),
                 None => {
                     self.prior.insert(target, prior);
                 }
-            }
+            };
         }
     }
 
-    fn generate_future_effects(&mut self, bb: &BasicBlockData<'tcx>) -> Vec<BasicBlockData<'tcx>> {
-        for _target in bb.terminator().successors() {
-            // debug!("target: {:?}", target);
+    fn generate_future_blocks(
+        &mut self,
+        bb: BasicBlock,
+        body: &Body<'tcx>,
+        future: &mut FxHashSet<BasicBlock>,
+    ) {
+        for target in body[bb].terminator().successors() {
+            if future.contains(&target) {
+                continue;
+            }
+            future.insert(target);
+            self.generate_future_blocks(target, body, future);
         }
-        return vec![];
     }
 }
